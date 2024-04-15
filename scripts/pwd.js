2f09632c4cc18eb33bdd6ab212fbe7386e9465be
KingCharlesVI <89871965+KingCharlesVI@users.noreply.github.com>
2024-04-15 15:07:46 +0100

    Optimisation
    - Added footers with version numbering to all pages
    - Decluttered number of files

--- scripts/pwd.js
+++
@@ -1,8 +0,0 @@
-var password = "NSW2024@";
-(function passcodeprotect() {
-   var passcode = prompt("Enter Password");
-   while (passcode !== password) {
-      alert("Incorrect Password");
-      return passcodeprotect();
-   }
-}());
